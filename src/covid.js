1081838ac184b78dafc77fc9f70852c0c42ffe8b
reymundadwi <reymunda613@gmail.com>
2021-10-05 02:33:14 +0700

    index.html responsive & implement owl carousel

--- src/covid.js
+++ src/covid.js
@@ -1,49 +1,90 @@
-import jquery, { nodeName } from 'jquery';
-window.$ = window.jQuery = jquery;
+import jquery from 'jquery';
 import './style/covid.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
-import '../node_modules/lightslider/dist/css/lightslider.css';
-require('lightslider');
 import axios from 'axios';
 
-$(document).ready(function() {
-    $("#light-slider").lightSlider({
-        pager: false,
-        autoWidth: false,
-        slideMargin: -100
-    });
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.min.css';
+
+window.$ = window.jQuery = jquery;
+const owlCarousel = require('owl.carousel');
+
+$(document).ready(() => {
+  $('.owl-carousel').owlCarousel({
+  });
 });
+// $(document).ready(function() {
+//     $("#light-slider").lightSlider({
+//         pager: false,
+//         autoWidth: false,
+//         slideMargin: -100
+//     });
+// });
 
-axios.get('https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=f236c6f23d13411ab924ce6172a37a67',{
-    params: {
-        country: 'id',
-        category: 'news',
-        apiKey: 'f236c6f23d13411ab924ce6172a37a67'
-
-    }
+axios.get('https://newsapi.org/v2/top-headlines?country=id&category=health&apiKey=f236c6f23d13411ab924ce6172a37a67', {
+  params: {
+    country: 'id',
+    category: 'news',
+    apiKey: 'f236c6f23d13411ab924ce6172a37a67',
+  },
 })
-    .then(res => {
-        const article = res.data.articles;
-        const articleContainer = document.querySelector('#light-slider')
-        article.forEach(e => {
-            const articleComponent =document.createElement('li');
-            const descWord = e.description.split(' ');
-            const croppedDesc = [];
-            for(let i = 0;i < 10;i++){
-                croppedDesc.push(descWord[i]);
-            }
-            articleComponent.innerHTML = `
-            <a href="${e.url}" target="_blank">
-                <div class=" col-md-3 news-item">
-                    <img src="${e.urlToImage}" width="100%" height="150px" alt="">
-                    <p>${e.title}</p>
-                    <p>${e.author}</p>
-                    <p>${croppedDesc.join(" ")}<span style="color: #71ACF0">...Read more</span></p>
+  .then((res) => {
+    const article = res.data.articles;
+    const articleContainer = document.querySelector('.owl-stage');
+    article.forEach((e) => {
+      const articleComponent = document.createElement('div');
+      articleComponent.classList.add('owl-item');
+      const descWord = e.description.split(' ');
+      const croppedDesc = [];
+      for (let i = 0; i < 10; i++) {
+        croppedDesc.push(descWord[i]);
+      }
+      articleComponent.innerHTML = `
+            
+                <div class="news-item">
+                <a href="${e.url}" target="_blank">
+                <img src="${e.urlToImage}" width="100%" height="150px" alt="">
+                <p>${e.title}</p>
+                <p>${e.author}</p>
+                <p>${croppedDesc.join(' ')}<span style="color: #71ACF0">...Read more</span></p>
+                </a>
+                
                 </div>
-            </a>
-          `
-            articleContainer.appendChild(articleComponent);
-        })
-        // console.log(res.data.articles);
-    })
+                `;
+      articleContainer.appendChild(articleComponent);
+    });
+    // console.log(res.data.articles);
+    if ($('.owl-carousel').hasClass('owl-theme')) { // resize event was triggering an error, this if statement is to go around it
+      $('.owl-carousel').trigger('destroy.owl.carousel'); // these 3 lines kill the owl, and returns the markup to the initial state
+      $('.owl-carousel').find('.owl-stage-outer').children().unwrap();
+      $('.owl-carousel').removeClass('owl-center owl-loaded owl-text-select-on');
+
+      $('.owl-carousel').owlCarousel({
+          items:3.25,
+          nav: true,
+          navText: ['<img src="./assets/previous.svg" class="me-5" role="presentation">','<img src="./assets/next.svg" role="presentation">'],
+          pagination: false,
+          responsive: {
+            0: {
+              items: 1.25,
+            },
+            576: {
+              items: 2.25,
+            },
+            768: {
+              items: 3,
+            },
+            1024: {
+              items: 3.25,
+            },
+            1280: {
+              items: 3.25,
+            }
+          }
+      }); // re-initialise the owl
+      document.querySelector('.owl-prev').addEventListener('click',() => {
+          console.log('tes')
+      })
+    }
+  });
